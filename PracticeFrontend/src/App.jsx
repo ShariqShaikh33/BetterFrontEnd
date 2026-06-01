4948ed00b5c57a46ccd4fce01039be819c9c8c31
Shariq Shaikh <shariqshaikh2705@gmail.com>
2026-06-01 16:20:50 +0530

    Created a smaple component

---
+++ PracticeFrontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Sample1 from "./components/Sample1"
+
+
+function App(){
+
+  return(
+  <div>
+    Hello
+    <Sample1/>
+  </div>
+  )
+}
+export default App
---
+++ PracticeFrontend/src/components/Sample1.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../App.css"
+
+function ButtonA(){
+  return(
+    <button>Button</button>
+  )
+}
+
+function Sample1() {
+  return (
+    <div>
+      <div className='div1'>Lets a Gooo!!</div>
+      <ButtonA/>
+    </div>
+  )
+}
+
+export default Sample1
